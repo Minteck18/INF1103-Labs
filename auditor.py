0a861040f2dcac8cfe17fd186acbda0395db7ce3
MinTeck <2601765@sit.singaporetech.edu.sg>
2026-09-10 22:03:37 +0800

    Added while loop to loop continously asking user for stock quantity until quit command is given

--- auditor.py
+++ auditor.py
@@ -1,9 +1,9 @@
 inventory = 0; 
 
 while True:
-   stock_quantity = input("Please enter the stock quantity")
-   if stock_quantity.lower() == "quit":
-      print("Bye")
+   stock_quantity = input("Please enter the stock quantity or type quit to exit")
+   if stock_quantity.strip().lower() == "quit":
+      print("You have exited from the program, bye")
       break     
     
 
