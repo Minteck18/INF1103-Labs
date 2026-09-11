c4c251e885e4d60bbddac5c6b3815924f0dc3598
MinTeck <2601765@sit.singaporetech.edu.sg>
2026-09-11 12:38:46 +0800

    Accept stock values as integers

--- auditor.py
+++ auditor.py
@@ -1,9 +1,19 @@
+#q1
 inventory = 0; 
 
+#q2
 while True:
-   stock_quantity = input("Please enter the stock quantity or type quit to exit")
-   if stock_quantity.strip().lower() == "quit":
-      print("You have exited from the program, bye")
-      break     
+    stock_quantity = input("Please enter the stock quantity or type quit to exit")
+    try:
+       #q2
+       if stock_quantity == "quit":
+             print("You have exited from the program, bye")
+             break     
+       # q3
+       stock_quantity = int(stock_quantity)
+  
+     
+
+    
     
 
