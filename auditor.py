69f4d51c9cb8fbc4f5ea71ca55805e348697742c
MinTeck <2601765@sit.singaporetech.edu.sg>
2026-09-11 16:16:30 +0800

    Reporting requirement added

--- auditor.py
+++ auditor.py
@@ -1,15 +1,12 @@
 #q1
 inventory = 0; 
 total = 0
+rejected_entries = 0
 
 #q2
 while True:
     stock_quantity = input("Please enter the stock quantity or type quit to exit")
     try:
-       #q2
-       if stock_quantity.lower() == "quit":
-           print("You have exited from the program, bye")
-           break
        # q3
        stock_quantity = int(stock_quantity)
        #q6
@@ -18,17 +15,23 @@ while True:
                print("current running total", total)
        #q5
        if stock_quantity < 0: 
+            rejected_entries += 1
             print("Negative numbers are not allowed!")
-
        #q7
        if total > 500:
             print("The inventory has exceed 500 units!")
             break
-       
     except:
+        #q8
+        if stock_quantity == "quit":
+                            print(f"Total Units Processed:",total)
+                            print(f"Number of failed/rejected entries", rejected_entries)
+                            break
         #q4
         if stock_quantity != stock_quantity.isdigit():
+            rejected_entries += 1
             print("Invalid input! Please enter a integer")
+        
      
 
     
