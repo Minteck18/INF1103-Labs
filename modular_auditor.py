9281224ca4fb146453ee1289b917be5dfc34f8a3
MinTeck <2601765@sit.singaporetech.edu.sg>
2026-09-19 15:32:33 +0800

    Completed the modular logic for all functions

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,34 +1,39 @@
-inventory = 0; 
-total = 0
-
 def get_valid_input():
+    inventory = 0;
     failed_attempts = 0
     while True:
-        stock_quantity = input("Please enter the stock quantity or type quit to exit")
-        stock_quantity = int(stock_quantity)
-        if stock_quantity > 0:
-               total += int(stock_quantity) 
-               print("current running total", total)
-        if stock_quantity < 0: 
-            failed_attempts += 1
-            print("Negative numbers are not allowed!")
-        if total > 500:
-            print("The inventory has exceed 500 units!")
-            break
-        if stock_quantity == "quit":
-           return "quit", failed_attempts
-        if stock_quantity != stock_quantity.isdigit():
-            failed_attempts += 1
-            print("Invalid input! Please enter a integer")
-        
-    return int(stock_quantity), failed_attempts
+        inventory = input("Please enter the stock quantity or type quit to exit")
+        if inventory.lower() == "quit":
+                return "quit", failed_attempts
+        if not inventory.isdigit():
+                failed_attempts += 1
+                print("Invalid input! Please enter a valid integer")
+                continue
+        return int(inventory), failed_attempts
 
 def process_delivery(current_total, new_value):    
-     return current_total + new_value
+     new_total = current_total + new_value
+     return new_total
 
 def calculate_tax(amount):
     return amount * 0.10
 
 def generate_report(total_units,failed_attempts):
-    print(f"Total Units Processed:",total_units)
-    print(f"Number of failed/rejected entries",failed_attempts)
+    print("This Is The Total Deliveries Processed:",total_units)
+    print("Number Of Failed/Rejected Entries:",failed_attempts)
+
+def main_function():
+    total_units  = 0
+    failed_attempts = 0
+
+    while True:
+        delivery, failed = get_valid_input()
+        failed_attempts += failed
+        if delivery == "quit":
+            break
+        total_units = process_delivery(total_units,delivery)
+        tax = calculate_tax(delivery)
+        print("The total tax for this delivery will be:",tax)
+    generate_report(total_units,failed_attempts)
+
+main_function()
